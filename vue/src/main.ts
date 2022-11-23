6eeb58bb97111802cc435e3cd34b198344759d73
rinkei <6terabyte.onlyreceive@gmail.com>
2022-11-23 20:42:48 +0900

    :sparkles:  element plus

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -1,10 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import './registerServiceWorker';
 import router from './router';
-import store from './store';
-import vuetify from './plugins/vuetify';
-import { loadFonts } from './plugins/webfontloader';
+import ElementPlus from 'element-plus';
+import '@/../node_modules/element-plus/dist/index.css';
 
-loadFonts();
-
-createApp(App).use(router).use(store).use(vuetify).mount('#app');
+createApp(App).use(router).use(ElementPlus).mount('#app');
